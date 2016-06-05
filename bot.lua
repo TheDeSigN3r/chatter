dd04f3be631434d35284169e97603ac70de0f9ac
TheDeSigN3r <s.h1459@yahoo.com>
2016-06-05 10:43:41 +0000

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -7,7 +7,7 @@ HTTPS = require('ssl.https')
 dofile('utilities.lua')
 ----config----
 local bot_api_key = "" --التوكم هنا
-local You =  --خلي ايدي حسابك
+local You = 87946267  --خلي ايدي حسابك
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[ ]]
